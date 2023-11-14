1ca401dd18bc3558b127c13e13d14dabef57f4e4
eugeniocachiombo <eugeniocachiombo@gmail.com>
2023-11-14 04:44:05 +0100

    Logo modificado

--- include/footHTML.php
+++ include/footHTML.php
@@ -1,6 +1,6 @@
 <footer class="bg-secondary text-white">
     <div class="d-flex justify-content-center align-items-center" style="min-height: inherit">
-    <b>Sistema Identificador de Raças de Caninas</b>
+    <i class="fas fa-shield-dog me-2" style="font-size: 30px"></i><b>Sistema Identificador de Raças de Caninas</b>
     </div>
 </footer>
 
