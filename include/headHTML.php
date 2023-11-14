5caf461874828ef395082d441f7306c8fa13e0f9
eugeniocachiombo <eugeniocachiombo@gmail.com>
2023-11-14 02:18:27 +0100

    Design feito

---
+++ include/headHTML.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/fontawesome/css/all5.css">
+    <link rel="stylesheet" href="assets/fontawesome/css/all6.css">
+    <link rel="stylesheet" href="assets/css/flat_picker.css">
+    <link rel="stylesheet" href="assets/css/tema_calendario.css">
+    <link rel="stylesheet" href="assets/css/estilo.css">
+    <link rel="stylesheet" href="assets/css/tabela.css">
+    <script src="assets/bootstrap/js/bootstrap.js"></script>
+    <script src="assets/jquery/jquery.js"></script>
+    <title>Página Incial</title>
+</head>
+<body>
---
+++ index.php
@@ -0,0 +1,85 @@
+<?php include 'include/headHTML.php';
+?>
+
+<h1>Sistema Identificador de Raças de Cães</h1>
+
+<div class='container mt-4 mb-4'>
+    <form action='' method='post' class='row g-3 d-flex justify-content-center'>
+        <div class="row border mt-4 bg-secondary text-white">
+            <div class="col">
+            <h3 class="text-center text-md-start p-2">Formulário de identificação</h3>
+            </div>
+        </div>
+
+        <div class='col-8 col-sm-4 col-md-4'>
+            <label for=''>Altura</label>
+            <select class='form-control' name='' id=''>
+                <option value=''>Selecione...</option>
+            </select>
+        </div>
+
+        <div class='col-8 col-sm-4 col-md-4'>
+            <label for=''>Cor</label>
+            <select class='form-control' name='' id=''>
+                <option value=''>Selecione...</option>
+            </select>
+        </div>
+
+        <div class='col-8 col-sm-4 col-md-4'>
+            <label for=''>Tamanho de Pelos</label>
+            <select class='form-control' name='' id=''>
+                <option value=''>Selecione...</option>
+            </select>
+        </div>
+
+        <div class='col-8 col-sm-4 col-md-4'>
+            <label for=''>Tamanho da Pata</label>
+            <select class='form-control' name='' id=''>
+                <option value=''>Selecione...</option>
+            </select>
+        </div>
+
+        <div class='col-8 col-sm-4 col-md-4'>
+            <label for=''>Tamanho da Cauda</label>
+            <select class='form-control' name='' id=''>
+                <option value=''>Selecione...</option>
+            </select>
+        </div>
+
+        <div class='col-8 col-sm-4 col-md-4'>
+            <label for=''>Tamanho do Focinho</label>
+            <select class='form-control' name='' id=''>
+                <option value=''>Selecione...</option>
+            </select>
+        </div>
+
+        <div class='col-8 col-sm-4 col-md-4'>
+            <label for=''>Tamanho da Cabeça</label>
+            <select class='form-control' name='' id=''>
+                <option value=''>Selecione...</option>
+            </select>
+        </div>
+
+        <div class='col-8 col-sm-4 col-md-4'>
+            <label for=''>Tamanho das Orelhas</label>
+            <select class='form-control' name='' id=''>
+                <option value=''>Selecione...</option>
+            </select>
+        </div>
+
+        <div class='col-8 col-sm-4 col-md-4'>
+            <label for=''>Tamanho dos Olhos</label>
+            <select class='form-control' name='' id=''>
+                <option value=''>Selecione...</option>
+            </select>
+        </div>
+
+        <div class='row pt-4 d-flex justify-content-center'>
+            <div class='col-8 col-sm-4 col-md-4'>
+                <input class='form-control bg-success text-light' type='submit' value='Indentificar'>
+            </div>
+        </div>
+    </form>
+</div>
+
+<?php include 'include/footHTML.php'; ?>
